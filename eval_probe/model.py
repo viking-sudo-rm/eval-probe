9c51ba10e77474974411ff3c59c378f342a8e340
Will Merrill <william.merrill@yale.edu>
2019-10-17 21:34:06 -0700

    Added @null_escaped.

--- eval_probe/model.py
+++ eval_probe/model.py
@@ -10,8 +10,21 @@ class Entity:
         self.father: Entity = None
 
     def __hash__(self):
-        """Hash this object by identity, not value."""
+        """Hash this object by identity, not value.
+
+        This is necessary if we want to add these objects to sets.
+        """
         return id(self)
+
+
+def null_escaped(predicate):
+    """Make a predicate return None if any arguments are None."""
+    def wrapped_predicate(*args):
+        if any(arg is None for arg in args):
+            return None
+        else:
+            return predicate(*args)
+    return wrapped_predicate
 
 
 john = Entity()
@@ -30,17 +43,18 @@ model_dict = {
     "mary": mary,
 
     # Define nouns, which map entity -> entity.
-    "mother": lambda x: x.mother,
-    "father": lambda x: x.father,
+    # TODO: Nouns should really be type entity -> truth_value.
+    "mother": null_escaped(lambda x: x.mother),
+    "father": null_escaped(lambda x: x.father),
 
     # Define intransitive verbs.
-    "cry": lambda x: x.is_crying,
-    "run": lambda x: x.is_running,
+    "cry": null_escaped(lambda x: x.is_crying),
+    "run": null_escaped(lambda x: x.is_running),
 
     # Define transitive verbs.
-    "like": lambda x, y: (x, y) in like,
-    "watch": lambda x, y: (x, y) in watch,
+    "like": null_escaped(lambda x, y: (x, y) in like),
+    "watch": null_escaped(lambda x, y: (x, y) in watch),
 
     # Define the copula.
-    "is": lambda x, y: x == y,
+    "is": null_escaped(lambda x, y: x == y),
 }
