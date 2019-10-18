3bba8d99d69bce91113c4e06171a2fcaece1d655
Will Merrill <william.merrill@yale.edu>
2019-10-17 21:24:45 -0700

    Added shared lexicon between both grammars.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from typing import List
 
-import nltk
 from nltk.grammar import FeatureGrammar
 from nltk.sem import interpret_sents
 from nltk.sem.logic import Expression
@@ -8,6 +7,17 @@ from nltk.sem.logic import ApplicationExpression, ConstantExpression
 from nltk.parse.generate import generate
 
 from eval_probe.model import model_dict
+
+
+def load_grammar(grammar_file: str,
+                 lexicon_file: str ="grammars/lexicon.fcfg") -> FeatureGrammar:
+    with open(grammar_file) as g_file:
+        grammar_text = g_file.read()
+        grammar_text += "\n\n"
+    with open(lexicon_file) as l_file:
+        grammar_text += l_file.read()
+
+    return FeatureGrammar.fromstring(grammar_text)
 
 
 def semantic_parse(sentences: List[List[str]],
@@ -29,12 +39,18 @@ def evaluate(expression: Expression, model_dict):
 
 
 def main():
-    grammar = nltk.data.load("grammars/basic_sents.fcfg", "fcfg")
+    grammar = load_grammar("grammars/basic_sents.fcfg")
     sentences = list(generate(grammar))
     expressions = semantic_parse(sentences, grammar)
     for expression in expressions:
         value = evaluate(expression, model_dict)
         print(str(expression), ":", value)
+
+    complex_grammar = load_grammar("grammars/complex_sents.fcfg")
+    for sentence in generate(complex_grammar):
+        print(sentence)
+        # This grammar has infinite capacity.
+        break
 
 
 if __name__ == "__main__":
